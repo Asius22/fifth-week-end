fe2e35dee91ed93f8ce08e733afe9b0031e6aaed
Asius22 <ciro_2000@live.it>
2022-04-16 12:20:20 +0200

    esercizio angular poca grafica

---
+++ TODO-LIST/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyListComponent } from './my-list/my-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TODO-LIST/src/app/my-list/my-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-list',
+  templateUrl: './my-list.component.html',
+  styleUrls: ['./my-list.component.scss']
+})
+export class MyListComponent {
+
+  todos: string[] = [];
+  input: string = "";
+  constructor() { }
+
+  btnClicked() {
+    if (this.input != "") {
+
+      this.todos.push(this.input)
+      this.input = ""
+    }
+  }
+
+  deleteElement(str: string) {
+    this.todos.forEach(
+      (element, index) => {
+        if (element == str) this.todos.splice(index, 1)
+      }
+    )
+  }
+}
